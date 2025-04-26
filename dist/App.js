7846416b5c4cee10aa82a31e1196765e7de0b495
mobin afshari <mma137813781378@gmail.com>
2025-04-26 21:59:43 +0330

    fix: should handle the socket just in main and handle others in another way, for now changes file with module geaph is sending to client

--- dist/App.js
+++ dist/App.js
@@ -2,14 +2,13 @@ import Button from "./Button.js";
 import Typography from "./Typogeraphy.js";
 
 export default function App() {
-  const socket = new WebSocket("ws://localhost:5173");
-
+  
   const app = document.createElement("div");
   app.style.textAlign = "center";
   app.style.padding = "20px";
-
+  
   const title = Typography({ text: "Hello World" });
-
+  
   const button = Button({
     text: "Click",
     onClick: () => {
@@ -17,35 +16,36 @@ export default function App() {
     },
   });
   button.id = "my-button";
-  socket.addEventListener("message", async (event) => {
-    const data = JSON.parse(event.data);
-    if (data.type === "reload") {
-      if (data.file.includes("Button.js")) {
-        const newModule = await import(`./Button.js?t=${Date.now()}`);
-        const newButton = newModule.default
-          ? newModule.default({
-              text: "Click",
-              onClick: () => {
-                alert("Button clicked");
-              },
-            })
-          : newModule.App();
+  // const socket = new WebSocket("ws://localhost:5173");
+  // socket.addEventListener("message", async (event) => {
+  //   const data = JSON.parse(event.data);
+  //   if (data.type === "reload") {
+  //     if (data.file.includes("Button.js")) {
+  //       const newModule = await import(`./Button.js?t=${Date.now()}`);
+  //       const newButton = newModule.default
+  //         ? newModule.default({
+  //             text: "Click",
+  //             onClick: () => {
+  //               alert("Button clicked");
+  //             },
+  //           })
+  //         : newModule.App();
 
-        const oldButton = document.getElementById("my-button");
-        oldButton.replaceWith(newButton);
-        newButton.id = "my-button";
-      }
-      if (data.file.includes("Typogeraphy.js")) {
-        const newModule = await import(`./Typogeraphy.js?t=${Date.now()}`);
-        const newTitle = newModule.default
-          ? newModule.default({ text: "Hello World" })
-          : newModule.App();
+  //       const oldButton = document.getElementById("my-button");
+  //       oldButton.replaceWith(newButton);
+  //       newButton.id = "my-button";
+  //     }
+  //     if (data.file.includes("Typogeraphy.js")) {
+  //       const newModule = await import(`./Typogeraphy.js?t=${Date.now()}`);
+  //       const newTitle = newModule.default
+  //         ? newModule.default({ text: "Hello World" })
+  //         : newModule.App();
 
-        const oldTitle = document.querySelector("h1");
-        oldTitle.replaceWith(newTitle);
-      }
-    }
-  });
+  //       const oldTitle = document.querySelector("h1");
+  //       oldTitle.replaceWith(newTitle);
+  //     }
+  //   }
+  // });
 
   app.appendChild(title);
   app.appendChild(button);
