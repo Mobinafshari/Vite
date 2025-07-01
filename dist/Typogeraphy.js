0c2fdae3a75d49ff642ab3eb383382af0f504dd4
Mobinafshari <mma137813781378@gmail.com>
2025-07-01 20:09:48 +0330

    fix: trying to implement vite and hmr from scratch

--- dist/Typogeraphy.js
+++
@@ -1,8 +0,0 @@
-export default function Typography({ text }) {
-  const title = document.createElement("h1");
-  title.textContent = text;
-  title.style.color = "#333";
-  title.style.fontSize = "2em";
-
-  return title;
-}
